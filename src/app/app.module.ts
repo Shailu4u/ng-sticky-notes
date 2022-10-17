37cd0b6c2d34e97df8f57079f3c26b79318c64c3
Shailendra Kumar <shailu.snist@gmail.com>
2022-10-17 20:00:58 +0530

    Added note container.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 import { AppComponent } from './app.component';
 import { NoteComponent } from './sticky-notes/note/note.component';
+import { NoteContainerComponent } from './sticky-notes/note-container/note-container.component';
 
 @NgModule({
-  declarations: [AppComponent, NoteComponent],
-  imports: [BrowserModule, DragDropModule],
+  declarations: [AppComponent, NoteComponent, NoteContainerComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    OverlayModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/sticky-notes/note-container/note-container.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-note-container',
+  templateUrl: './note-container.component.html',
+  styleUrls: ['./note-container.component.scss'],
+})
+export class NoteContainerComponent implements OnInit {
+  isOpen = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
